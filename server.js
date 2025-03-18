1dcd07a7d20463004f44a93044e2e63b89ef303d
harsh-gupta02 <harsh.gupta@affordplan.com>
2025-03-18 16:55:06 +0530

    still in development

--- server.js
+++ server.js
@@ -10,11 +10,19 @@ const port = process.env.PORT || 3000
 
 app.use(cors())
 
-const toString = (req, res, next) => {
+const toString = (data) => {
+    if (typeof data !== 'string') {
+        req.body.url = JSON.stringify(data)
+    }
+}
+const validateType = (req, res, next) => {
     if (req.body) {
-        if (typeof req.body.url !== 'string') {
-            req.body.url = JSON.parse(req.body.url)
-            console.log("1: ",req.body)
+        if(req.body.url) {
+            toString(req.body.url)
+        }
+        else if(req.body.text) {
+            req.body.url = req.body.data
+            delete req.body.data
         }
         next()
     } else {
@@ -26,10 +34,10 @@ app.get('/', (req, res) => {
     res.send('Qr Genie Server')
 })
 
-app.post('/generate', toString, async (req, res) => {
+app.post('/generate', validateType, async (req, res) => {
     try {
         const data = req.body.url
-        const qrCode = await qrcode.toDataURL(data)
+        const qrCode = await qrcode.toDataURL(data, {width: 165})
         res.send({ qrCode })
     }
     catch (error) {
